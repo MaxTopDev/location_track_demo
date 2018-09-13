47cefefca941c552ad35732ab8bf9d8e11f90710
Max O <ohm@unicsoft.com.ua>
2018-09-13 14:34:49 +0300

    initial commit

---
+++ LocationTrackingDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocationTrackingDemo
+//
+//  Created by developer MacBook on 9/13/18.
+//  Copyright © 2018 Unicsoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
